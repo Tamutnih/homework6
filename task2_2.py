8eb07b35ce29cdffc85a0f535e4a55107566f56d
Tamutnih <renu07@list.ru>
2022-10-14 20:31:40 +0500

    solution

---
+++ task2_2.py
@@ -0,0 +1,25 @@
+# Задача №2
+# Создать текстовый файл (не программно),
+# сохранить в нем несколько строк,
+# выполнить подсчет количества строк,
+# количества слов в каждой строке.
+
+
+def calc_lines_words(file_path):
+
+    result = dict()
+    line_num = 0
+    try:
+        with open(file_path, 'r') as f:
+            for line_num, fl in enumerate(f, 1):
+                result[line_num] = len(fl.split())
+    except Exception as err:
+        print(err)
+    return line_num, result
+
+
+if __name__ == '__main__':
+    num, counts = calc_lines_words('task2.txt')
+    print(f'Found {num} line(s)')
+    for k, v in counts.items():
+        print(f'Line {k}: {v} word(s)')
